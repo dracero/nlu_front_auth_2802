d5b180d5f25e0b9c41f63c16cbe8716b9f9b9a09
dracero <diego.racero@gmail.com>
2022-03-28 17:20:58 -0300

    second

--- src/Login.js
+++ src/Login.js
@@ -2,9 +2,21 @@ import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 
 const Login = () => {
+  const auth = () =>{
+    let myHeaders = new Headers();
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+   fetch("https://backend0322.herokuapp.com/auth/google", requestOptions)
+   .then(response => response.text())
+   .then(result => console.log(result))
+   .catch(error => console.log('error', error));
+  }
+
   return(
     <Box m={0} pt={0}>
-      <Button href="https://backend0322.herokuapp.com/auth/google" variant="outlined">Login</Button>
+      <Button onClick={auth()} variant="outlined">Login</Button>
     </Box>
   )
 };
